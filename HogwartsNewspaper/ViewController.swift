37a7a216cbbf38601fbc2e4eeed036d468df94c1
Shubham Mishra <shubhammishra10@gmail.com>
2021-04-24 19:08:11 +0530

    ADD: Detect harry potter image

--- HogwartsNewspaper/ViewController.swift
+++ HogwartsNewspaper/ViewController.swift
@@ -21,12 +21,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,6 +28,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.detectionImages = ARReferenceImage.referenceImages(inGroupNamed: "News Images", bundle: .main)
+        configuration.maximumNumberOfTrackedImages = .max
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -47,15 +43,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 
     // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
+
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
+        
+        guard let imageAnchor = anchor as? ARImageAnchor else { return nil }
+        
+        let refImage = imageAnchor.referenceImage
+        
+        let imagePlane = SCNPlane(width: refImage.physicalSize.width, height: refImage.physicalSize.height)
+        
+        let imageNode = SCNNode(geometry: imagePlane)
+        imageNode.eulerAngles.x = -.pi/2
+        node.addChildNode(imageNode)
+        
+        if refImage.name == "harry-potter" {
+            print("harry potter image detected")
+        }
      
         return node
     }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
